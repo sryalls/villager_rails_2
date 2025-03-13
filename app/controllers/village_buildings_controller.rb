5c5d51093a4a3a9b0ff49dfb7c9c57544d94367a
Sandy Ryalls <sandy.ryalls@googlemail.com>
2025-03-13 15:00:25 +0000

    feat: building create controller deducts resources on build

--- app/controllers/village_buildings_controller.rb
+++ app/controllers/village_buildings_controller.rb
@@ -3,11 +3,21 @@ class VillageBuildingsController < ApplicationController
   before_action :set_village
 
   def create
-    @building = Building.find(params[:village_building][:building_id])
-    @village.buildings << @building
-    respond_to do |format|
-      format.html { redirect_to @village, notice: "Building was successfully added." }
-      format.turbo_stream
+    @building = Building.find(village_building_params[:building_id])
+    resource_params = village_building_params[:resources]
+
+    if sufficient_resources?(resource_params)
+      deduct_resources(resource_params)
+      @village.buildings << @building
+      respond_to do |format|
+        format.html { redirect_to @village, notice: "Building was successfully added." }
+        format.turbo_stream
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to @village, alert: "Insufficient resources to build this building." }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("flash", partial: "shared/flash", locals: { alert: "Insufficient resources to build this building." }) }
+      end
     end
   end
 
@@ -15,5 +25,23 @@ class VillageBuildingsController < ApplicationController
 
   def set_village
     @village = Village.find(params[:village_id])
+  end
+
+  def village_building_params
+    params.require(:village_building).permit(:building_id, resources: {})
+  end
+
+  def sufficient_resources?(resource_params)
+    resource_params.to_h.all? do |resource_id, quantity|
+      village_resource = @village.village_resources.find_by(resource_id: resource_id)
+      village_resource && village_resource.count >= quantity.to_i
+    end
+  end
+
+  def deduct_resources(resource_params)
+    resource_params.to_h.each do |resource_id, quantity|
+      village_resource = @village.village_resources.find_by(resource_id: resource_id)
+      village_resource.update(count: village_resource.count - quantity.to_i)
+    end
   end
 end
